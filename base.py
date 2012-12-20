df8d8176bccd67d0ffd5735997e15a65a0ad16ba
David Brownman <beamneocube@gmail.com>
2012-12-19 22:11:55 -0500

    Changed mana and hp so that they work w/ max. Added basically all of the basic items.

--- base.py
+++ base.py
@@ -52,8 +52,9 @@ class Champion(object):
 
     def setBase(self):
         # self.cur_stats 
-        self.cur_stats = {'hp':0,'hp_max':0,'mana_max':0,'hpreg':0,'mana':0,'manareg':0,'ad':0,'ap':0,'ms':0,'as':0,'armor':0,'mr':0,'crit':0,
-            'lifesteal':0,'spellvamp':0,'flat_armor_pen':0,'flat_magic_pen':0,'perc_armor_pen':0,'perc_magic_pen':0,'cdr':0}
+        self.cur_stats = {'hp':0,'hp_max':0,'mana_max':0,'hpreg':0,'mana':0,'manareg':0,'ad':0,'ap':0,'ms':0,'as':0,'armor':0,
+            'mr':0,'crit':0,'lifesteal':0,'spellvamp':0,'flat_armor_pen':0,'flat_magic_pen':0,'perc_armor_pen':0,'perc_magic_pen':0,
+                'cdr':0,'damage_block':0,'onhit':{}}
 
     def resetStats(self):
         for s in self.cur_stats:
@@ -127,7 +128,8 @@ class Ninja(Champion):
     def setBase(self):
         # self.cur_stats 
         self.cur_stats = {'hp':0,'hp_max':0,'hpreg':0,'energy':0,'ad':0,'ap':0,'ms':0,'as':0,'armor':0,'mr':0,'crit':0,
-            'lifesteal':0,'spellvamp':0,'flat_armor_pen':0,'flat_magic_pen':0,'perc_armor_pen':0,'perc_magic_pen':0,'cdr':0}
+            'lifesteal':0,'spellvamp':0,'flat_armor_pen':0,'flat_magic_pen':0,'perc_armor_pen':0,'perc_magic_pen':0,
+                'cdr':0,'damage_block':0,'onhit':{}}
 
     def energy(self, val=0):
         if val:
