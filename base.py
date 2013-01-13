df0b834dfb8a74004ac02250e076af18e244a54e
David Brownman <beamneocube@gmail.com>
2013-01-13 17:27:54 -0500

    part of the way through changing abilities. going to change what the ability functions return so we can have both non damaging abilities and stuff

--- base.py
+++ base.py
@@ -55,7 +55,12 @@ class Champion(object):
         self.cur_stats = {'level':0,'hp':0,'hp_max':0,'mana_max':0,'hp_regen':0,'mana':0,'mana_regen':0,'ad':0,'ap':0,'ms':0,'as':0,'armor':0,
             'mr':0,'crit':0,'lifesteal':0,'spellvamp':0,'flat_armor_pen':0,'flat_magic_pen':0,'perc_armor_pen':0,'perc_magic_pen':0,
                 'cdr':0,'damage_block':0,'on_hit':{},'on_self_hit':{},'status':{},'cooldowns':{'i':0,'q':0,'w':0,'e':0,'r':0},'ability_rank':{'q':0,'w':0,'e':0,'r':0}}
-        # pprint(self.cur_stats)
+        #for debugging so that all the abilities are up: 
+        for a in self.cur_stats['ability_rank']:
+            if a == 'r':
+                self.cur_stats['ability_rank'][a] = 3
+            else:
+                self.cur_stats['ability_rank'][a] = 5
 
     def resetStats(self):
         for s in self.cur_stats:
@@ -78,53 +83,35 @@ class Champion(object):
                         continue
         self.cur_stats['hp'] = self.cur_stats['hp_max']
         # self.cur_stats['mana'] = self.cur_stats['mana_max'] #FIX FOR NINJA
-    def useAbility(self,ability,dtype=False):
-        if 'on' in self.c['moves'][ability]: #marks whether or not the ability is a toggle
-            if self.c['moves'][ability]['on'] == True: #toggling off
-                self.c['moves'][ability]['on'] = False
-                self.setCooldowns(ability)
-            elif self.c['moves'][ability]['on'] == False: #toggling on|| technically some abilities have a cooldown when you turn them on but i wont sweat that now
-                self.c['moves'][ability]['on'] = True
-
-        if ability == 'i':
-            damage = self.i()
-        elif ability == 'q':
-            damage = self.q()
-        elif ability == 'w':
-            damage = self.w()
-        elif ability == 'e':
-            damage = self.e()
-        elif ability == 'r':
-            damage = self.r()
-        if 'on' not in self.c['moves'][ability]:
-            self.setCooldowns(ability)
-        
-        if self.ninja:
-            self.energy(-(self.c['moves'][ability]['cost_val'][5]))
-        else:
-            self.mana(-(self.c['moves'][ability]['cost_val'][5]))
-        return damage
 
     def q(self, dtype = False):
         if (dtype):
             return self.c['moves']['q']['damage_type']
         else:
-            return moveMult(self.c['moves']['q']['damage'],5,self.cur_stats[self.c['moves']['q']['damage_ratio_type']],self.c['moves']['q']['damage_ratio']) 
+            return moveMult(self.c['moves']['q']['damage'],self.cur_stats['ability_rank']['q'],
+                self.cur_stats[self.c['moves']['q']['damage_ratio_type']],self.c['moves']['q']['damage_ratio']) 
+
     def w(self, dtype = False):
         if (dtype):
             return self.c['moves']['w']['damage_type']
         else:
-            return moveMult(self.c['moves']['w']['damage'],5,self.cur_stats[self.c['moves']['w']['damage_ratio_type']],self.c['moves']['w']['damage_ratio']) 
+            return moveMult(self.c['moves']['w']['damage'],self.cur_stats['ability_rank']['w'],
+                self.cur_stats[self.c['moves']['w']['damage_ratio_type']],self.c['moves']['w']['damage_ratio']) 
+
     def e(self, dtype = False):
         if (dtype):
             return self.c['moves']['e']['damage_type']
         else:
-            return moveMult(self.c['moves']['e']['damage'],5,self.cur_stats[self.c['moves']['e']['damage_ratio_type']],self.c['moves']['e']['damage_ratio']) 
+            return moveMult(self.c['moves']['e']['damage'],self.cur_stats['ability_rank']['e'],
+                self.cur_stats[self.c['moves']['e']['damage_ratio_type']],self.c['moves']['e']['damage_ratio']) 
+
     def r(self, dtype = False):
         if (dtype):
             return self.c['moves']['r']['damage_type']
         else:
-            return moveMult(self.c['moves']['r']['damage'],3,self.cur_stats[self.c['moves']['r']['damage_ratio_type']],self.c['moves']['r']['damage_ratio'])
+            return moveMult(self.c['moves']['r']['damage'],self.cur_stats['ability_rank']['r'],
+                self.cur_stats[self.c['moves']['r']['damage_ratio_type']],self.c['moves']['r']['damage_ratio'])
+
     def ad(self):
         return self.cur_stats['ad']
     def ap(self):
@@ -155,34 +142,52 @@ class Champion(object):
         self.hpRegen()
         self.secondaryRegen()
         self.cooldowns()
-        # print 'normal tick'
     def hpRegen(self):
         if self.cur_stats['hp'] < self.cur_stats['hp_max']:
             self.hp(self.cur_stats['hp_regen']/5.0)
-            # print 'regened '+str(self.cur_stats['hp_regen']/5.0)
     def secondaryRegen(self): #this is named as such as to account for fury and energy so I don't always need to redefine tick()
         if self.cur_stats['mana'] < self.cur_stats['mana_max']:
             self.mana(self.cur_stats['mana_regen']/5.0)
     def cooldowns(self):
-        # pprint(self.cur_stats)
         for a in self.cur_stats['cooldowns']:
             if self.cur_stats['cooldowns'][a] > 0:
                self.cur_stats['cooldowns'][a] -= 1
     def setCooldowns(self,ability):
-        self.cur_stats['cooldowns'][ability] = self.c['moves'][ability]['cd'][5]
-    def canCast(self,ability):
-        # pprint(self.c['moves'][ability]['cost_val'][5])
-        # print 'cost: '+str(self.c['moves'][ability]['cost_val'][5])
-        # print 'current mana: '+str(self.cur_stats[self.c['moves'][ability]['cost_type']])
-        # print 'cooldown: '+str(self.cur_stats['cooldowns'][ability])
-        if (self.c['moves'][ability]['cost_val'][5] < self.cur_stats[self.c['moves'][ability]['cost_type']] and self.cur_stats['cooldowns'][ability] == 0):
-            # print 'True'
-            return True
+        self.cur_stats['cooldowns'][ability] = self.c['moves'][ability]['cd'][self.cur_stats['ability_rank'][ability]]
+    def canCast(self,ability): #this will at some point need to account for being silenced
+        if (self.c['moves'][ability]['cost_val'][self.cur_stats['ability_rank'][ability]] < self.cur_stats[self.c['moves'][ability]['cost_type']] 
+            and self.cur_stats['cooldowns'][ability] == 0):
+                return True
         else:
-            # print 'False'
             if 'on' in self.c['moves'][ability]:
                 self.c['moves'][ability]['on'] = False
             return False
+
+    def useAbility(self,ability):
+        if self.canCast(ability):#if you can cast
+            if self.ninja: #spend energy if ninja
+                self.energy(-(self.c['moves'][ability]['cost_val'][self.cur_stats['ability_rank'][ability]]))
+            else: #spend mana
+                self.mana(-(self.c['moves'][ability]['cost_val'][self.cur_stats['ability_rank'][ability]]))
+
+        if 'on' in self.c['moves'][ability]: #decides whether or not the ability is a toggle one
+            if self.c['moves'][ability]['on'] == True: #toggling off
+                self.c['moves'][ability]['on'] = False
+                self.setCooldowns(ability)
+            elif self.c['moves'][ability]['on'] == False: #toggling on|| technically some abilities have a cooldown when you turn them on but i wont sweat that now
+                self.c['moves'][ability]['on'] = True
+        else: self.setCooldowns(ability)
+
+        if ability == 'i':
+            damage = self.i()
+        elif ability == 'q':
+            damage = self.q()
+        elif ability == 'w':
+            damage = self.w()
+        elif ability == 'e':
+            damage = self.e()
+        elif ability == 'r':
+            damage = self.r()
 
 class Ninja(Champion):
     def __init__(self,cd):
@@ -194,6 +199,11 @@ class Ninja(Champion):
         self.cur_stats = {'level':0,'hp':0,'hp_max':0,'hp_regen':0,'energy':0,'ad':0,'ap':0,'ms':0,'as':0,'armor':0,'mr':0,'crit':0,
             'lifesteal':0,'spellvamp':0,'flat_armor_pen':0,'flat_magic_pen':0,'perc_armor_pen':0,'perc_magic_pen':0,
                 'cdr':0,'damage_block':0,'on_hit':{},'on_self_hit':{},'status':{},'cooldowns':{'i':0,'q':0,'w':0,'e':0,'r':0},'ability_rank':{'q':0,'w':0,'e':0,'r':0}}
+        for a in self.cur_stats['ability_rank']:
+            if a == 'r':
+                self.cur_stats['ability_rank'][a] = 3
+            else:
+                self.cur_stats['ability_rank'][a] = 5
 
     def energy(self, val=0):
         if val:
@@ -216,7 +226,8 @@ class Ahri(Champion):
 
     # also may want to change this response from an int to json?
     def q2(self):
-        return moveMult(self.c['moves']['q']['damage_2'],5,self.cur_stats[self.c['moves']['q']['damage_ratio_type']],self.c['moves']['q']['damage_2_ratio'])
+        return moveMult(self.c['moves']['q']['damage_2'],self.cur_stats['ability_rank']['q'],
+            self.cur_stats[self.c['moves']['q']['damage_ratio_type']],self.c['moves']['q']['damage_2_ratio'])
     
 
 class Akali(Ninja):
@@ -230,7 +241,7 @@ class Akali(Ninja):
         if (dtype):
             return self.c['moves']['e']['damage_type']
         else:
-            return moveMult(self.c['moves']['e']['damage'],5,self.cur_stats[self.c['moves']['e']['damage_ratio_type']],
+            return moveMult(self.c['moves']['e']['damage'],self.cur_stats['ability_rank']['e'],self.cur_stats[self.c['moves']['e']['damage_ratio_type']],
                 self.c['moves']['e']['damage_ratio'],self.cur_stats[self.c['moves']['e']['damage_ratio_type_b']],self.c['moves']['e']['damage_ratio_b'])
 
 class Alistar(Champion):
@@ -241,7 +252,8 @@ class Alistar(Champion):
         if (dtype):
             return self.c['moves']['e']['damage_type']
         else:
-            return moveMult(self.c['moves']['e']['self_heal_val'],5,self.cur_stats[self.c['moves']['e']['heal_ratio_type']],self.c['moves']['e']['self_heal_ratio'])
+            return moveMult(self.c['moves']['e']['self_heal_val'],self.cur_stats['ability_rank']['e'],
+                self.cur_stats[self.c['moves']['e']['heal_ratio_type']],self.c['moves']['e']['self_heal_ratio'])
 
 class Amumu(Champion):
     def __init__(self,cd):
@@ -251,8 +263,9 @@ class Amumu(Champion):
         if (dtype):
             return self.c['moves']['w']['damage_type']
         else:
-            return (moveMult(self.c['moves']['w']['damage_b'],5,self.cur_stats[self.c['moves']['w']['damage_ratio_type_b']],self.c['moves']['w']['damage_ratio_b']) + 
-                self.c['moves']['w']['damage'][5])
+            return (moveMult(self.c['moves']['w']['damage_b'],self.cur_stats['ability_rank']['w'],
+                self.cur_stats[self.c['moves']['w']['damage_ratio_type_b']],self.c['moves']['w']['damage_ratio_b']) 
+                + self.c['moves']['w']['damage'][self.cur_stats['ability_rank']['w']])
 
 
     def tick(self):
